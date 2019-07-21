df4a20c9ff2d805a8d7cbffe8a42d8f83fe14cdf
seregant <indraagus@live.com>
2019-07-21 14:04:33 +0700

    add create db for container

--- database/database.go
+++ database/database.go
@@ -5,13 +5,15 @@ import (
 	"log"
 
 	"github.com/seregant/cockroach-test/config"
+	"github.com/seregant/cockroach-test/hash"
 	"github.com/seregant/cockroach-test/structs"
 
 	"github.com/jinzhu/gorm"
 )
 
+var conf = config.SetConfig()
+
 func DbConnect() *gorm.DB {
-	var conf = config.SetConfig()
 	var addr = "postgresql://" + conf.User + "@" + conf.Host + ":" + conf.Port + "/" + conf.DbName + "?sslmode=disable"
 	db, err := gorm.Open("postgres", addr)
 	if err != nil {
@@ -25,6 +27,8 @@ func DbInit() {
 	var db = DbConnect()
 	defer db.Close()
 
+	db.Exec("CREATE DATABASE " + conf.DbName)
+
 	fmt.Println("Creating tables...")
 
 	db.AutoMigrate(&structs.Jabatan{})
@@ -35,13 +39,13 @@ func DbInit() {
 
 	fmt.Println("Initiating tables...")
 
-	// db.Create(&structs.Jabatan{NamaJabatan: "direktur"})
-	// db.Create(&structs.Jabatan{NamaJabatan: "manager"})
-	// db.Create(&structs.Jabatan{NamaJabatan: "supervisor"})
-	// db.Create(&structs.Jabatan{NamaJabatan: "staff"})
+	db.Create(&structs.Jabatan{IDJabatan: "JB" + hash.GenerateIDData(), NamaJabatan: "direktur"})
+	db.Create(&structs.Jabatan{IDJabatan: "JB" + hash.GenerateIDData(), NamaJabatan: "manager"})
+	db.Create(&structs.Jabatan{IDJabatan: "JB" + hash.GenerateIDData(), NamaJabatan: "supervisor"})
+	db.Create(&structs.Jabatan{IDJabatan: "JB" + hash.GenerateIDData(), NamaJabatan: "staff"})
 
-	// db.Create(&structs.Divisi{NamaDivisi: "keuangan"})
-	// db.Create(&structs.Divisi{NamaDivisi: "hrd"})
-	// db.Create(&structs.Divisi{NamaDivisi: "sales"})
-	// db.Create(&structs.Divisi{NamaDivisi: "teknis"})
+	db.Create(&structs.Divisi{IDDivisi: "DV" + hash.GenerateIDData(), NamaDivisi: "keuangan"})
+	db.Create(&structs.Divisi{IDDivisi: "DV" + hash.GenerateIDData(), NamaDivisi: "hrd"})
+	db.Create(&structs.Divisi{IDDivisi: "DV" + hash.GenerateIDData(), NamaDivisi: "sales"})
+	db.Create(&structs.Divisi{IDDivisi: "DV" + hash.GenerateIDData(), NamaDivisi: "teknis"})
 }
